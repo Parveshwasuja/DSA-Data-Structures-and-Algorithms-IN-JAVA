00cf7e4e36828aefe81c7e4411da0ed5b57a4edf
parvesh <parveshwasuja@gmail.com>
2020-10-05 00:22:12 +0530

    find last index of element by recursion

---
+++ Foundation/1.Basics of programming/last_index.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class last_index {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        int i;
+        for(i=0;i<n;i++){
+            a[i] = sc.nextInt();
+        }
+        int x = sc.nextInt();
+        System.out.println(lastIndex(a,0,x));
+    }
+
+    public static int lastIndex(int[] arr, int idx, int x){
+        if(idx==arr.length){
+            return -1;
+        }
+        int li = lastIndex(arr,idx+1,x);
+        if(li!=-1){
+            return li;
+        }else{
+            if(arr[idx]==x){
+                return idx;
+            }
+            return li;
+        }
+        
+    }
+
+}
