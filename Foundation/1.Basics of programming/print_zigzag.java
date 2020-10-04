e6948ffe397d7623bfe770dc6c033e07fc866241
parvesh <parveshwasuja@gmail.com>
2020-10-04 23:20:51 +0530

    Recursion

---
+++ Foundation/1.Basics of programming/print_zigzag.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class print_zigzag {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        pzz(n);
+    }
+
+    public static void pzz(int n){
+        if(n==1){
+            System.out.print(n+" "+n+" "+n+" ");
+            return;
+        }else{
+            System.out.print(n+" ");
+            pzz(n-1);
+            System.out.print(n+" ");
+            pzz(n-1);
+            System.out.print(n+" ");
+        }
+    }
+
+}
