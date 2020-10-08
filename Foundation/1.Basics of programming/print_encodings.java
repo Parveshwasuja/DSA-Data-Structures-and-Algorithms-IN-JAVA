bf733397093eca9b585f133c27f34fe409650f3c
parvesh <parveshwasuja@gmail.com>
2020-10-08 17:48:22 +0530

    Recursion with only printing,not storing in arraylist

---
+++ Foundation/1.Basics of programming/print_encodings.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class print_encodings {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        printEncodings(s,"");
+    }
+    
+    static char[] codes = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+    public static void printEncodings(String str,String ans) {
+        
+        if(str.length()==0){
+            System.out.println(ans);
+            return;
+        }
+        if(str.charAt(0)=='0'){
+            return;
+        }
+        
+        char ch = str.charAt(0);
+        int t = ch - 48;
+        String ros = str.substring(1);
+        printEncodings(ros,ans+codes[t-1]);
+        if(str.length()>=2){
+           String ch2 = str.substring(0,2);
+           int z = Integer.valueOf(ch2);
+            if(z<=26){
+                printEncodings(str.substring(2),ans+codes[z-1]);
+            }
+        }
+        
+        
+    }
+
+}
---
+++ Foundation/1.Basics of programming/print_permutations.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class print_permutations {
+
+    public static void main(String[] args) throws Exception {
+            
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        printPermutations(s,"");
+        
+    }
+
+    public static void printPermutations(String str,String ans) {
+        
+        if(str.length()==0){
+            System.out.println(ans);
+            return;
+        }
+        
+        for(int i=0;i<str.length();i++){
+            StringBuilder sb = new StringBuilder(str);
+            char ch = sb.charAt(i);
+            sb.deleteCharAt(i);
+            printPermutations(sb.toString(),ans+ch);
+        }
+        
+        
+    }
+
+}
