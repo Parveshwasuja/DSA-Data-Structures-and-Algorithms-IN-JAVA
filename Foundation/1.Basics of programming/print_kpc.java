d5bbc487a80aa528cb9d69190a667fe96be8473c
parvesh <parveshwasuja@gmail.com>
2020-10-08 17:47:01 +0530

    Recursion with only printing,not storing in arraylist

---
+++ Foundation/1.Basics of programming/print_kpc.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class print_kpc {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        printKPC(s,"");
+    }
+    static String[] codes = {".;","abc","def","ghi","jkl","mno","pqrs","tu","vwx","yz"};
+    public static void printKPC(String str,String ans) {
+        
+        if(str.length()==0){
+            System.out.println(ans);
+            return;
+        }
+        char ch = str.charAt(0);
+        int idx = ch - 48;
+        String ros = str.substring(1);
+        for(int i=0;i<codes[idx].length();i++){
+            printKPC(ros,ans+codes[idx].charAt(i));
+        }
+        
+    }
+
+}
---
+++ Foundation/1.Basics of programming/print_maze_paths_jumps.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class print_maze_paths_jumps {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        printMazePaths(1,1,n,m,"");
+        
+    }
+
+    // sr - source row
+    // sc - source column
+    // dr - destination row
+    // dc - destination column
+    public static void printMazePaths(int sr, int sc, int dr, int dc, String path) {
+        
+        if(sr==dr && sc==dc){
+            System.out.println(path);
+            return;
+        }
+        if(sr>dr || sc>dc){
+            return;
+        }
+        
+        for(int i=1;i<=dc-sc;i++){
+            printMazePaths(sr,sc+i,dr,dc,path+"h"+String.valueOf(i));
+        }
+        for(int i=1;i<=dr-sr;i++){
+            printMazePaths(sr+i,sc,dr,dc,path+"v"+String.valueOf(i));
+        }
+        int limit;
+        if(sr==dr || sc==dc)limit=0;
+        else limit = Math.min(dr,dc) - Math.min(sr,sc);
+        for(int i=1;i<=limit;i++){
+            printMazePaths(sr+i,sc+i,dr,dc,path+"d"+String.valueOf(i));
+        }
+        
+    }
+
+}
